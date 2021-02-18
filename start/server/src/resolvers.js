397b13d0a9bd32e97d13070d7fbca96c2ddf4f6d
Andres Osante <aosantea@ucenfotec.ac.cr>
2021-02-17 23:07:02 -0600

    add pagination to laucnhes query

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -1,3 +1,4 @@
+const {paginateResults} = require('./utils')
 // A resolver is a function that's responsible for populating the data for a single field in your schema.
 // Whenever a client queries for a particular field, the resolver for that field
 // fetches the requested data from the appropriate data source.
@@ -5,8 +6,26 @@
 // arguments of query resolves: (parent, args, context, info)
 module.exports = {
   Query: {
-    launches: (_, __, { dataSources }) =>
-      dataSources.launchAPI.getAllLaunches(),
+    launches: async (_, { pageSize = 20, after }, { dataSources }) => {
+      const allLaunches = await dataSources.launchAPI.getAllLaunches();
+      // we want these in reverse chronological order
+      allLaunches.reverse();
+      const launches = paginateResults({
+        after,
+        pageSize,
+        results: allLaunches
+      });
+      return {
+        launches,
+        cursor: launches.length ? launches[launches.length - 1].cursor : null,
+        // if the cursor at the end of the paginated results is the same as the
+        // last item in _all_ results, then there are no more results after this
+        hasMore: launches.length
+          ? launches[launches.length - 1].cursor !==
+            allLaunches[allLaunches.length - 1].cursor
+          : false
+      };
+    },
     launch: (_, { id }, { dataSources }) =>
       dataSources.launchAPI.getLaunchById({ launchId: id }),
     me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser(),
