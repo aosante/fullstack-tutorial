82b12d57d3a0bc8575c5cd75851fd1700ea89bf9
aosante <aosantea@ucenfotec.ac.cr>
2021-03-28 23:18:14 -0600

    set up apollo client and queries

--- start/client/src/pages/profile.tsx
+++ start/client/src/pages/profile.tsx
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+import { Loading, Header, LaunchTile } from '../components';
+import { LAUNCH_TILE_DATA } from './launches';
 import { RouteComponentProps } from '@reach/router';
+import * as GetMyTripsTypes from './__generated__/GetMyTrips';
+
+export const GET_MY_TRIPS = gql`
+  query GetMyTrips {
+    me {
+      id
+      email
+      trips {
+        ...LaunchTile
+      }
+    }
+  }
+  ${LAUNCH_TILE_DATA}
+`;
 
 interface ProfileProps extends RouteComponentProps {}
 
 const Profile: React.FC<ProfileProps> = () => {
-  return <div />;
+  const {
+    data,
+    loading,
+    error
+  } = useQuery<GetMyTripsTypes.GetMyTrips>(
+    GET_MY_TRIPS,
+
+    // If you query for data that's already present in your cache, Apollo Client can return that data without needing to fetch it over the network.
+    /*
+    By setting this query's fetch policy to network-only, we guarantee that Apollo Client always queries our 
+    server to fetch the user's most up-to-date list of booked trips
+    */
+    { fetchPolicy: "network-only" }
+  );
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR: {error.message}</p>;
+  if (data === undefined) return <p>ERROR</p>;
+
+  return (
+    <Fragment>
+      <Header>My Trips</Header>
+      {data.me && data.me.trips.length ? (
+        data.me.trips.map((launch: any) => (
+          <LaunchTile key={launch.id} launch={launch} />
+        ))
+      ) : (
+        <p>You haven't booked any trips</p>
+      )}
+    </Fragment>
+  );
 }
 
 export default Profile;
